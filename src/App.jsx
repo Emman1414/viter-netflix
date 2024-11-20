9bc45b7d2976cb155c4dffe8b01a4d0a4e5ecdb7
liempot <leemagno9@gmail.com>
2024-11-20 14:37:33 +0800

    wowanggalingngarawmo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { imgPath } from './components/helpers/functions-general'
+
+const App = () => {
+  return (
+    <>
+      <div>
+        aasdasda
+      </div>
+    </>
+  )
+}
+
+export default App
